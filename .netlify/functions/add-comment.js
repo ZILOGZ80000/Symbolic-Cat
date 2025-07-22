70574866bddb35b51e71c7c8ec77e4efe48d2447
ZILOGZ80000 <boarovvoloda@gmail.com>
2025-07-22 19:18:42 +0400

    Create add-comment.js

---
+++ .netlify/functions/add-comment.js
@@ -0,0 +1,35 @@
+exports.handler = async (event) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'https://symbolic-cat.netlify.app',
+    'Access-Control-Allow-Credentials': 'true'
+  };
+
+  try {
+    // Парсим тело запроса (например, JSON с сообщением чата)
+    const requestBody = JSON.parse(event.body);
+
+    const response = await fetch(${process.env.DB_URL}/chats, {
+      method: 'POST',
+      headers: {
+        'token': process.env.DB_KEY,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(requestBody)
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: response.status,
+      headers,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: error.message || 'Internal Server Error' })
+    };
+  }
+};
