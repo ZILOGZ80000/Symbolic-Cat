5ff0a589979852fa6a2372014d4f8670c6618b45
ZILOGZ80000 <boarovvoloda@gmail.com>
2025-07-26 13:33:38 +0400

    Update login-user.js

--- .netlify/functions/login-user.js
+++ .netlify/functions/login-user.js
@@ -84,8 +84,7 @@ exports.handler = async (event) => {
 
     // ставим куки
     headers['Set-Cookie'] = 
-      `session=${sessionId}; Path=/; Secure; SameSite=Strict; HttpOnly; Max-Age=3600;\n` + 
-      `username=${encodeURIComponent(username)}; Path=/; Secure; SameSite=Strict; Max-Age=3600;`;
+      `session=${sessionId}; Path=/; Secure; SameSite=Strict; HttpOnly; Max-Age=3600;\nusername=${encodeURIComponent(username)}; Path=/; Secure; SameSite=Strict; Max-Age=3600;`;
 
     return {
       statusCode: 200,
